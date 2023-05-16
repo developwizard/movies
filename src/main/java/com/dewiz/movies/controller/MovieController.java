4550a0f66497d63a50038a2a9d03bab003c0dbae
dewiz <churkin.it@gmail.com>
2023-05-17 00:11:20 +0300

    Implemented finding movie by imdbId

--- src/main/java/com/dewiz/movies/controller/MovieController.java
+++ src/main/java/com/dewiz/movies/controller/MovieController.java
@@ -5,10 +5,12 @@ import com.dewiz.movies.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,5 +21,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @GetMapping("{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId) {
+        return ResponseEntity.ok(movieService.getMovieByImdbId(imdbId));
     }
 }
--- src/main/java/com/dewiz/movies/service/MovieService.java
+++ src/main/java/com/dewiz/movies/service/MovieService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +14,9 @@ public class MovieService {
     private final MovieRepository movieRepository;
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieByImdbId(String id) {
+        return movieRepository.findByImdbId(id);
     }
 }
